44e679f05672a65a805847a791dac210b07111f5
ahmedbnissa <abenaissa@welyne.com>
2024-07-29 16:29:08 +0100

    coding with Java

---
+++ src/main/java/com/example/demo3/models/DataCenter.java
@@ -0,0 +1,50 @@
+package com.example.demo3.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="DataCenters")
+
+public class DataCenter {
+	 @Id
+	    @GeneratedValue(strategy = GenerationType.IDENTITY) 
+      private Long DataCenterId;
+      public Long getDataCenterId() {
+		return DataCenterId;
+	}
+	public void setDataCenterId(Long dataCenterId) {
+		DataCenterId = dataCenterId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getCapacity() {
+		return capacity;
+	}
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
+	public List<Server> getServers() {
+		return servers;
+	}
+	public void setServers(List<Server> servers) {
+		this.servers = servers;
+	}
+	private String name;
+      private int capacity;
+      @OneToMany(mappedBy = "DataCenter", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+      private List<Server> servers = new ArrayList<>();
+}
---
+++ src/main/java/com/example/demo3/models/Server.java
@@ -0,0 +1,47 @@
+package com.example.demo3.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="Servers")
+public class Server {
+	    @Id
+	    @GeneratedValue(strategy = GenerationType.IDENTITY) 
+	    private Long id; 
+	    private String name; 
+	    private String OS ; 
+	    @ManyToOne
+	    @JoinColumn(name = "DataCenterId", referencedColumnName = "DatacenterId")
+	    private DataCenter DataCenter;
+		public Long getId() {
+			return id;
+		}
+		public void setId(Long id) {
+			this.id = id;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getOS() {
+			return OS;
+		}
+		public void setOS(String oS) {
+			OS = oS;
+		}
+		public DataCenter getData_center() {
+			return DataCenter;
+		}
+		public void setData_center(DataCenter data_center) {
+			this.DataCenter = data_center;
+		} 
+
+}
