ba051befe3054d0eb0b7e455750fbacf837d7947
ahmedbnissa <abenaissa@welyne.com>
2024-08-06 23:13:59 +0100

    server apis

--- src/main/java/com/example/demo3/controllers/ServerController.java
+++ src/main/java/com/example/demo3/controllers/ServerController.java
@@ -1,5 +1,29 @@
 package com.example.demo3.controllers;
 
-public class ServerController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-}
+
+import com.example.demo3.models.Server;
+
+import com.example.demo3.repositories.ServerService;
+@RestController
+@RequestMapping("/servers")
+public class ServerController {
+	@Autowired
+    ServerService ss;
+	 @PostMapping("/add")
+	 @PreAuthorize("hasRole('USER')")
+	  Server newDataCenter(@RequestBody Server newServer,@RequestParam("data_center_id") Long datacenterid) {
+	    if(ss.addServer(newServer,datacenterid)==true) {
+	    	return newServer;
+	    }
+	    else {
+	    	return null;
+	    }
+}}
